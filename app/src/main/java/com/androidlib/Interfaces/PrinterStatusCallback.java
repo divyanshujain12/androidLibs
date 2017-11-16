d1c1205456c19d07710519bf3e6a62effc614693
divyanshuPC <divyanshujain12@hotmail.com>
2017-11-16 23:38:19 +0530

    printer integration

---
+++ app/src/main/java/com/androidlib/Interfaces/PrinterStatusCallback.java
@@ -0,0 +1,9 @@
+package com.androidlib.Interfaces;
+
+/**
+ * Created by divyanshuPC on 11/16/2017.
+ */
+
+public interface PrinterStatusCallback {
+    void status(String message, String battery);
+}
