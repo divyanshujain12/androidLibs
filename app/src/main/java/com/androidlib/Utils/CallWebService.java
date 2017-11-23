f01116d768fcf12a931dab477bc34474bbf25f06
divyanshu.jain <divyanshujain12@hotmail.com>
2017-11-23 16:30:52 +0530

    font changes

--- app/src/main/java/com/androidlib/Utils/CallWebService.java
+++ app/src/main/java/com/androidlib/Utils/CallWebService.java
@@ -6,8 +6,8 @@ import android.util.Base64;
 
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.RetryPolicy;
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.Response.Listener;
 import com.android.volley.error.AuthFailureError;
 import com.android.volley.error.NetworkError;
 import com.android.volley.error.NoConnectionError;
@@ -19,8 +19,9 @@ import com.android.volley.request.JsonArrayRequest;
 import com.android.volley.request.JsonObjectRequest;
 import com.androidlib.CustomViews.CustomToasts;
 import com.androidlib.GlobalClasses.LibInit;
-import com.androidlib.Interfaces.LibConstants;
-import com.locationlib.R;
+import com.androidlib.Utils.InternetCheck;
+import com.locationlib.R.string;
+import com.locationlib.R.style;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -29,37 +30,33 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-
-/**
- * Created by Divyanshu jain on 30-10-2015.
- */
-public class CallWebService implements Response.ErrorListener, Response.Listener {
-
+public class CallWebService implements ErrorListener, Listener {
     private Context context = null;
-
-    public static int GET = Request.Method.GET;
-    public static int POST = Request.Method.POST;
-    public static int PUT = Request.Method.PUT;
-    public static int DELETE = Request.Method.DELETE;
-    private ObjectResponseCallBack objectCallBackInterface;
-    private ArrayResponseCallback arrayCallBackInterface;
+    public static int GET = 0;
+    public static int POST = 1;
+    public static int PUT = 2;
+    public static int DELETE = 3;
+    private CallWebService.ObjectResponseCallBack objectCallBackInterface;
+    private CallWebService.ArrayResponseCallback arrayCallBackInterface;
     private int apiCode = 0;
     private String url = "";
-    //private Snackbar continuousSB;
-    private static String username, password, authKey;
+    private static String username;
+    private static String password;
+    private static String authKey;
     private ProgressDialog progressDialog;
+
+    public CallWebService() {
+    }
+
     public static CallWebService getInstance(Context context, boolean showProgressBar, int apiCode) {
         CallWebService instance = new CallWebService();
         instance.context = context;
         instance.apiCode = apiCode;
-        //instance.continuousSB = null;
-        if (context != null && showProgressBar){
-            instance.progressDialog = new ProgressDialog(context,R.style.MyAlertDialogStyle);
+        if (context != null && showProgressBar) {
+            instance.progressDialog = new ProgressDialog(context, style.MyAlertDialogStyle);
             instance.progressDialog.setCancelable(false);
             instance.progressDialog.setMessage("Loading Data..");
-        }
-
-        else{
+        } else {
             instance.progressDialog = null;
         }
 
@@ -69,131 +66,123 @@ public class CallWebService implements Response.ErrorListener, Response.Listener
     public void setHeadersValue(String userid, String pass, String authKey) {
         username = userid;
         password = pass;
-        authKey = authKey;
     }
-    public void hitJsonObjectRequestAPI(int requestType, final String url, JSONObject json, final ObjectResponseCallBack callBackInterface) {
-        if (InternetCheck.isInternetOn(context)) {
-            objectCallBackInterface = callBackInterface;
-            cancelRequest(url);
-            this.url = url;
-            if (progressDialog != null)
-                progressDialog.show();
 
-            JsonObjectRequest request = new JsonObjectRequest(requestType, url, json == null ? null : (json), this, this){
-                @Override
+    public void hitJsonObjectRequestAPI(final int requestType, final String url, JSONObject json, CallWebService.ObjectResponseCallBack callBackInterface) {
+        if (InternetCheck.isInternetOn(this.context)) {
+            this.objectCallBackInterface = callBackInterface;
+            this.cancelRequest(url);
+            this.url = url;
+            if (this.progressDialog != null) {
+                this.progressDialog.show();
+            }
+
+            JsonObjectRequest request = new JsonObjectRequest(requestType, url, json == null ? null : json, this, this) {
                 public Map<String, String> getHeaders() throws AuthFailureError {
-                    Map<String, String> params = new HashMap<String, String>();
-                    String credentials = username + ":" + password;
-                    String auth = "Basic "
-                            + Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP);
+                    HashMap params = new HashMap();
+                    String credentials = CallWebService.username + ":" + CallWebService.password;
+                    String auth = "Basic " + Base64.encodeToString(credentials.getBytes(), 2);
                     params.put("Content-Type", "application/json");
                     params.put("Authorization", auth);
                     return params;
                 }
             };
-            addRequestToVolleyQueue(url, request);
+            this.addRequestToVolleyQueue(url, request);
         } else {
-            CustomToasts.getInstance(context).showErrorToast(context.getString(R.string.no_internet_connection));
+            CustomToasts.getInstance(this.context).showErrorToast(this.context.getString(string.no_internet_connection));
         }
+
     }
 
-    public void hitJsonArrayRequestAPI(int requestType, final String url, JSONArray json, final ArrayResponseCallback callBackinerface) {
-        arrayCallBackInterface = callBackinerface;
-        cancelRequest(url);
-
+    public void hitJsonArrayRequestAPI(int requestType, String url, JSONArray json, CallWebService.ArrayResponseCallback callBackinerface) {
+        this.arrayCallBackInterface = callBackinerface;
+        this.cancelRequest(url);
         this.url = url;
+        if (this.progressDialog != null) {
+            this.progressDialog.show();
+        }
 
-        if (progressDialog != null)
-            progressDialog.show();
-
-        JsonArrayRequest request = new JsonArrayRequest(requestType, url, json == null ? null : (json), this, this);
-        addRequestToVolleyQueue(url, request);
+        JsonArrayRequest request = new JsonArrayRequest(requestType, url, json == null ? null : json, this, this);
+        this.addRequestToVolleyQueue(url, request);
     }
 
     private void addRequestToVolleyQueue(String url, Request request) {
-        RetryPolicy policy = new DefaultRetryPolicy(LibConstants.REQUEST_TIMEOUT_TIME, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        DefaultRetryPolicy policy = new DefaultRetryPolicy(30000, 0, 1.0F);
         request.setRetryPolicy(policy);
         LibInit.getInstance().addToRequestQueue(request, url);
     }
 
-    @Override
     public void onErrorResponse(VolleyError error) {
-        LibInit.getInstance().getRequestQueue().getCache().invalidate(url, true);
-        onError(configureErrorMessage(error));
+        LibInit.getInstance().getRequestQueue().getCache().invalidate(this.url, true);
+        this.onError(configureErrorMessage(error));
     }
 
-    @Override
     public void onResponse(Object response) {
-        LibInit.getInstance().getRequestQueue().getCache().invalidate(url, true);
-        if (progressDialog != null)
-            progressDialog.hide();
+        LibInit.getInstance().getRequestQueue().getCache().invalidate(this.url, true);
+        if (this.progressDialog != null) {
+            this.progressDialog.hide();
+        }
 
         if (response instanceof JSONObject) {
-            onJsonObjectResponse((JSONObject) response);
+            this.onJsonObjectResponse((JSONObject) response);
         } else if (response instanceof JSONArray) {
-            onJsonArrayResponse((JSONArray) response);
+            this.onJsonArrayResponse((JSONArray) response);
         }
 
     }
 
     private void onJsonObjectResponse(JSONObject response) {
         try {
-            if (response.keys().hasNext()) {
-                String key = response.keys().next();
-                if (response.get(key) != null) {
-                    if (objectCallBackInterface != null)
-                        objectCallBackInterface.onJsonObjectSuccess(response, apiCode);
-                } else {
-                    objectCallBackInterface.onFailure("Data Not Available!", apiCode);
-                }
-            } else {
-                objectCallBackInterface.onFailure("Data Not Available!", apiCode);
-            }
-//
-        } catch (final JSONException e) {
-            onError(e.getMessage());
-            e.printStackTrace();
+            if (response.has("error")) {
+                this.objectCallBackInterface.onFailure("Some Error Occured", this.apiCode);
+            } else
+                checkNullValue(response);
+        } catch (JSONException var3) {
+            this.onError(var3.getMessage());
+            var3.printStackTrace();
         }
+
     }
 
+    private void checkNullValue(JSONObject response) throws JSONException {
+        String e = "";
+        if (response.keys().hasNext()) {
+            e = (String) response.keys().next();
+            if (response.get(e) != null && response.get(e) != "null") {
+                if (this.objectCallBackInterface != null) {
+                    this.objectCallBackInterface.onJsonObjectSuccess(response, this.apiCode);
+                }
+            } else {
+                this.objectCallBackInterface.onFailure("Data Not Available for" + e, this.apiCode);
+            }
+        } else {
+            this.objectCallBackInterface.onFailure("Data Not Available for" + e, this.apiCode);
+        }
+    }
 
     private void onJsonArrayResponse(JSONArray response) {
         try {
             if (response.get(0) != null) {
-                arrayCallBackInterface.onJsonArraySuccess(response, apiCode);
+                this.arrayCallBackInterface.onJsonArraySuccess(response, this.apiCode);
             } else {
-                arrayCallBackInterface.onFailure("Data Not Available!", apiCode);
+                this.arrayCallBackInterface.onFailure("Data Not Available!", this.apiCode);
             }
-        } catch (final JSONException e) {
-            onError(e.getMessage());
-            e.printStackTrace();
+        } catch (JSONException var3) {
+            this.onError(var3.getMessage());
+            var3.printStackTrace();
         }
-    }
-
-
-    public interface ObjectResponseCallBack {
-
-        void onJsonObjectSuccess(JSONObject response, int apiType) throws JSONException;
-
-        void onFailure(String str, int apiType);
-
-    }
-
-    public interface ArrayResponseCallback {
-
-        void onJsonArraySuccess(JSONArray array, int apiType) throws JSONException;
-
-        void onFailure(String str, int apiType);
 
     }
 
     private void onError(String error) {
+        if (this.progressDialog != null) {
+            this.progressDialog.hide();
+        }
 
-        if (progressDialog != null)
-            progressDialog.hide();
-        if (objectCallBackInterface != null)
-            objectCallBackInterface.onFailure(error, apiCode);
-        //  CustomToasts.getInstance(context).showErrorToast(error);
+        if (this.objectCallBackInterface != null) {
+            this.objectCallBackInterface.onFailure(error, this.apiCode);
+        }
+
     }
 
     public static String configureErrorMessage(VolleyError volleyError) {
@@ -210,28 +199,30 @@ public class CallWebService implements Response.ErrorListener, Response.Listener
             message = "Cannot connect to Internet...Please check your connection!";
         } else if (volleyError instanceof TimeoutError) {
             message = "Connection TimeOut! Please check your internet connection.";
+        } else if (volleyError.networkResponse != null && volleyError.networkResponse.data != null) {
+            VolleyError error = new VolleyError(new String(volleyError.networkResponse.data));
+            message = error.getMessage();
         }
-        else{
-            if (volleyError.networkResponse != null && volleyError.networkResponse.data != null) {
-                VolleyError error = new VolleyError(new String(volleyError.networkResponse.data));
-                message = error.getMessage();
-            }
-        }
+
         return message;
     }
 
     private void cancelRequest(String url) {
-        if (this.url.equals(url))
+        if (this.url.equals(url)) {
             LibInit.getInstance().cancelPendingRequests(url);
+        }
+
     }
 
-   /* private void sendErrorToView() {
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-            }
-        };
-        handler.postDelayed(runnable, 2000);
+    public interface ArrayResponseCallback {
+        void onJsonArraySuccess(JSONArray var1, int var2) throws JSONException;
+
+        void onFailure(String var1, int var2);
     }
-    Handler handler = new Handler();*/
+
+    public interface ObjectResponseCallBack {
+        void onJsonObjectSuccess(JSONObject var1, int var2) throws JSONException;
+
+        void onFailure(String var1, int var2);
+    }
 }
