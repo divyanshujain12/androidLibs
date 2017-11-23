920f931a7f627d73b9839b6994080a8bebcd8314
divyanshu.jain <divyanshujain12@hotmail.com>
2017-11-23 12:59:40 +0530

    font changes

--- app/src/main/java/com/androidlib/Utils/CallWebService.java
+++ app/src/main/java/com/androidlib/Utils/CallWebService.java
@@ -203,7 +203,7 @@ public class CallWebService implements Response.ErrorListener, Response.Listener
         } else if (volleyError instanceof ServerError) {
             message = "The server could not be found. Please try again after some time!!";
         } else if (volleyError instanceof AuthFailureError) {
-            message = "Cannot connect to Internet...Please check your connection!";
+            message = "Invalid Credential";
         } else if (volleyError instanceof ParseError) {
             message = "Parsing error! Please try again after some time!!";
         } else if (volleyError instanceof NoConnectionError) {
